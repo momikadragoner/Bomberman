134b0f4e9713b7a66d9cc69e15acabe6e7bdbb89
momikadragoner <molnarmate2002@gmail.com>
2024-02-17 20:36:02 +0100

    Add project files.

---
+++ Model/Tiles/Wall.cs
@@ -0,0 +1,12 @@
+﻿namespace Bomberman.Model.Tiles
+{
+    public class Wall : Tile
+    {
+        public Wall(int x, int y) : base(x, y)
+        {
+            Walkable = false;
+        }
+
+        public override void Blown() { } // nothing happens when blown up
+    }
+}
